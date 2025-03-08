6c778742693a69107fdcdd8b07ede0fe356a47ba
linweijia <2138388112@qq.com>
2025-03-09 00:50:44 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import ClipboardList from './components/ClipboardList.vue'
+</script>
+
+<template>
+   <ClipboardList />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/ClipboardList.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <img src="../assets/vue.svg">
+</template>
+
+<style scoped>
+/* 暂时保留空样式标签 */
+</style>
